07a14f7c46eab551874c7d0bca3965abb4c33f43
Nicolas Hervy <nicolas@hervy.se>
2023-01-16 00:09:10 +0100

    SCSS added, all set.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'bryggan vid Stegerholmen'
+export const SITE_DESCRIPTION = 'En sajt för att hantera Sterholmsbryggan'
